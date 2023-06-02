6645de0c28211f9c3607eb9a83a674e834cc8ecd
CodeBeans2022 <jamieleekinnear21@gmail.com>
2023-06-02 16:35:53 +0200

    update

--- code.js
+++ code.js
@@ -1,34 +1,78 @@
-let item = document.querySelector('#item');
+let itemInput = document.querySelector('#item');
 let output = document.querySelector('#output');
-
+let id = 1;
 let list = [];
+let addItemBtn = document.querySelector('#addItem');
 
-function addItem() {
+addItemBtn.addEventListener('click', (e) => {
+    event.preventDefault();
     list.push({
         id: '',
-        name: item.value,
-        createdDate: '',
-        completed: ''
-    })
+        name: itemInput.value,
+        createdDate: new Date(),
+        completed: false
+    });
+    id++;
+    output.innerHTML = '';
+    itemInput.value = '';
+    render();
+})
 
-    for(let i = 0; i < 0; i++) {
-        
-    }
+// function addItem() {
+    // console.log(list);
+    // console.log(list[0].createdDate);
+// }
+
+function sortItems() {
     
-    console.log(list);
+}
+
+function deleteBtn() {
+    let deleteBtns = [...document.querySelectorAll('#delete')];
+    deleteBtns.forEach((item) => {
+        item.addEventListener('click', (e) => {
+            list.splice((parseInt(deleteBtns.indexOf(item))), 1);
+            console.log(list);
+        })
+    })
+}
+
+
+function render() {
+    
+    output.innerHTML = '';
 
     list.forEach((item) => {
         output.innerHTML += `
         <div class="listItem">
         <input type="checkbox">
         <h2 class="itemHeading">${item.name}</h2>
-        <button id="delete">X</button>
+        <button id="delete" onclick="deleteBtn()">X</button>
         </div>
         `
     })
-    
 }
 
-function sortItems() {
 
-}
+// String Methods
+
+// let names = ["Jason", "John", "James"];
+
+// for(let i = 0; i < names.length; i++ ) {
+//     if(names[i] == "Jason") {
+//         console.log(`Found at position ${i}`);
+//     }
+// }
+
+// console.log(names.filter (name => {
+//     return name.charAt(1) == "a"
+// }));
+
+// console.log(names[0].charAt(2));
+
+// let data = [3, "Beans", 7];
+
+// let addition = data.filter(d => {
+//     return Number.isInteger(d)
+// }).reduce((a,b) => {return a+b})
+// console.log(addition);
